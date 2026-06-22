6d6d8fb56b34028f454ba34de26d971fbe0052cc
LuisMezarina <diegomezarina.provincias@gmail.com>
2026-06-21 23:48:48 -0500

    Elementos asignados

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const ingresado = document.getElementById("a-convertir") 
+const metrosConvertir = document.getElementById("metros1")
+const kilometrosConvertir = document.getElementById("kilometros1")
+const millasConvertir = document.getElementById("millas1")
+const celciusConvertir = document.getElementById("celcius1")
+const farenheitConvertir = document.getElementById("farenheit1")
+const metrosObtenido = document.getElementById("metros2")
+const millasObtenido = document.getElementById("millas2")
+const kilometrosObtenido = document.getElementById("kilometros2")
+const celciusObtenido = document.getElementById("celcius2")
+const farenheitObtenido = document.getElementById("farenheit2")
+
+let boton = document.getElementById("calcular")
 
 
 
+function proceso() {
+    const distancia = metrosConvertir.value
+}
 
-ingresado = addEventListener
+
+let resultado = boton.addEventListener("click", proceso)
